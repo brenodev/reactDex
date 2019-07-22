6b44fe83af989ddd66bb548c795954d6a4d84618
Breno Guilherme de Souza Lopes <bg.souza.lopes@hotmail.com>
2019-07-22 13:22:00 -0300

    adicionado dependencia axios e criado chamada api

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,19 +1,36 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 // Custom Components 
 import PokemonCard from '../PokemonCard'
 
 class PokemonList extends Component {
+
+  state = {
+    url: 'https://pokeapi.co/api/v2/pokemon',
+    pokemon: null,
+  }
+
+  async componentDidMount() {
+    const res = await axios(this.state.url)
+    this.setState({
+      pokemon: res.data['results']
+    })
+  }
+
   render() {
     return (
-      <div className="row">
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
-        <PokemonCard />
-      </div>
+      <React.Fragment>
+        {this.state.pokemon ? (
+          <div className="row">
+            {this.state.pokemon.map(pokemon => (
+              <PokemonCard />
+            ))}
+          </div>
+        ) : (
+          <h1>Loading Pokémon</h1>
+        )}
+      </React.Fragment>
     )
   }
 }
